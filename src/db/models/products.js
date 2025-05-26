c4de8757d10da667d35cfc1b6797457163d57a2b
Oleksii Ozyrskyi <aozyrskyii@gmail.com>
2025-05-26 16:18:19 +0200

    Filtering and pagination of products

--- src/db/models/products.js
+++ src/db/models/products.js
@@ -1,48 +1,49 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model } from "mongoose";
 
 const productSchema = new Schema(
-    {
-        name: { type: String, required: true },
-        sku: { type: String, default: '' },
-        volumeOptions: {
-            type: [String],
-            required: true,
-        },
-        priceByVolume: [
-            {
-                volume: { type: String, required: true },
-                price: { type: Number, required: true },
-            }
-        ],
-        stockQuantity: {
-            type: Number,
-            required: true,
-            default: 0,
-        },
-        features: {
-            type: [String],
-            required: true,
-        },
-        description: String,
-        instructions: String,
-        activeIngredients: [
-            {
-                name: String,
-                description: String,
-            },
-        ],
-        inciList: [String],
-        category: {
-            type: String,
-            enum: ['hair', 'face', 'body', 'makeup', 'home'],
-            default: 'hair',
-            required: true,
-        },
-        isVegan: { type: Boolean, default: false },
-        imageUrl: String,
-        inStock: { type: Boolean, default: true },
+  {
+    name: { type: String, required: true },
+    sku: { type: String, default: "" },
+    volumeOptions: {
+      type: [String],
+      required: true,
     },
-    { timestamps: true, versionKey: false }
+    priceByVolume: [
+      {
+        volume: { type: String, required: true },
+        price: { type: Number, required: true },
+      },
+    ],
+    stockQuantity: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    features: {
+      type: [String],
+      required: true,
+    },
+    description: String,
+    instructions: String,
+    activeIngredients: [
+      {
+        name: String,
+        description: String,
+      },
+    ],
+    inciList: [String],
+    category: {
+      type: String,
+      enum: ["hair", "face", "body", "makeup", "home"],
+      default: "hair",
+      required: true,
+    },
+    isVegan: { type: Boolean, default: false },
+    isPromoted: { type: Boolean, default: false },
+    imageUrl: String,
+    inStock: { type: Boolean, default: true },
+  },
+  { timestamps: true, versionKey: false }
 );
 
-export const ProductModel = model('products', productSchema);
+export const ProductModel = model("products", productSchema);
